c06815d397d6da62f79c95a7f53dc31d0d013e67
gslavchev <georgi.slavchev@landmark.co.uk>
2018-02-08 03:23:53 +0200

    @ dis . I know I am gonna photo this!

--- src/app/services/instagram.service.ts
+++ src/app/services/instagram.service.ts
@@ -22,7 +22,7 @@ export class InstagramService {
         
         let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: cpHeaders });
-        console.log(postUrl +"/"+ code);
+        console.log(postUrl + code);
 
         return this.http.get(url)
         .map(this.extractData)
@@ -41,7 +41,7 @@ export class InstagramService {
                 }
             }
         _body = _body.match(dataExp)[1]
-        //console.log(JSON.parse(_body).entry_data.PostPage[0].graphql)
+        console.log(JSON.parse(_body).entry_data.PostPage[0].graphql.shortcode_media)
         return ({
             pro_pic: JSON.parse(_body)
                 .entry_data.PostPage[0].graphql.shortcode_media.owner.profile_pic_url,
